e732c0d6cf48b27349777b848576597191a52326
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-12-28 22:35:22 +0000

    minor gui patch 1

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -809,7 +809,7 @@ void SendCoinsDialog::coinControlChangeEdited(const QString& text)
         }
         else if (!IsValidDestination(dest)) // Invalid address
         {
-            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Bitcoin address"));
+            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid MinersWorldCoin address"));
         }
         else // Valid address
         {
