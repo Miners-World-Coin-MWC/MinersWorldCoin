dceeaa3fb44b3f1340e2a61b25e98db3f32c93b5
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-12-31 00:07:23 +0000

    minor patch

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -47,8 +47,8 @@ uint256 CBlockHeader::GetPoWHash() const
         .version = YESPOWER_1_0, 
         .N = 2048, 
         .r = 32, 
-        .pers = (const uint8_t *)"Let the quest begin",
-        .perslen = 19
+        .pers = (const uint8_t *)"Mining made easy and accessible to all - Miners World Coin 2025",
+        .perslen = 63
     };
 
     uint32_t time = le32dec(&ss[68]);
